0eb4d8e0e0fda43de826ce37c415e9c1c10ce110
rotaerk <m.scott.stewart@gmail.com>
2022-05-13 02:23:30 -0400

    Added pre-existing NixOS configuration.

--- flake.nix
+++ flake.nix
@@ -1,30 +1,10 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
-
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.amaethon = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules =
-        [ ({ pkgs, ... }: {
-            boot.isContainer = true;
-
-            # Let 'nixos-version --json' know about the Git revision
-            # of this flake.
-            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-            # Network configuration.
-            networking.useDHCP = false;
-            networking.firewall.allowedTCPPorts = [ 80 ];
-
-            # Enable a web server.
-            services.httpd = {
-              enable = true;
-              adminAddr = "morty@example.org";
-            };
-          })
-        ];
+      modules = [ ./configuration.nix ];
     };
-
   };
 }
